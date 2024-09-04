a1aacc0b1c5e6aa12ce86112af05f177e3235f47
Keshav S <55992029+Keshav13142@users.noreply.github.com>
2024-09-04 17:38:58 +0530

    remove env vars validation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,17 +3,10 @@ import path from "path";
 import { defineConfig } from "vite";
 import { ValidateEnv } from "@julr/vite-plugin-validate-env";
 import { z } from "zod";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 export default defineConfig({
   plugins: [
     react(),
-    ValidateEnv({
-      validator: "zod",
-      schema: {
-        VITE_BACKEND_URL: z.string().url(),
-      },
-    }),
     TanStackRouterVite(),
   ],
   resolve: {
