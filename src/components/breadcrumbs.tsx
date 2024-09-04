07e27e9cb34c0f568c77ae81da4fb678d7e740a7
Keshav S <55992029+Keshav13142@users.noreply.github.com>
2024-09-04 17:06:50 +0530

    Create breadcrumbs.tsx

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,44 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Link } from "@tanstack/react-router";
+import { Slash } from "lucide-react";
+import { Fragment } from "react";
+
+type BreadcrumbItemProps = {
+  title: string;
+  link: string;
+};
+
+export function Breadcrumbs({ items }: { items: BreadcrumbItemProps[] }) {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {items.map((item, index) => (
+          <Fragment key={item.title}>
+            {index !== items.length - 1 && (
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link to={item.link}>{item.title}</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            )}
+            {index < items.length - 1 && (
+              <BreadcrumbSeparator>
+                <Slash />
+              </BreadcrumbSeparator>
+            )}
+            {index === items.length - 1 && (
+              <BreadcrumbPage>{item.title}</BreadcrumbPage>
+            )}
+          </Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
